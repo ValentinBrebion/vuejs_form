a3df2e38685746ac73c67725126781d7634e5bb2
Vincent Kraus <vincent.kraus@etudiant.univ-lr.fr>
2023-12-25 15:54:21 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <form>
+    <div class="form-control">
+      <label for="id" class="label">Identifiant</label>
+      <input type="text" class="input input-bordered" name="id" id="" />
+    </div>
+
+    <div class="form-control">
+      <label for="pwd" class="label">Mot de passe</label>
+      <input type="password" class="input input-bordered" name="pwd" id="" />
+    </div>
+
+    <button type="button" class="btn btn-primary mt-4">Se connecter</button>
+  </form>
+</template>
